d9b2dc90b4a8cbb50447a0ad271deef6308aa307
unknown <giedriusbanaitis@gmail.com>
2012-08-14 12:02:35 +0300

    added better support for IIS ataching

--- Whut.AttachTo/AttachToPackage.cs
+++ Whut.AttachTo/AttachToPackage.cs
@@ -2,9 +2,13 @@
 using System.ComponentModel.Design;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 using EnvDTE;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Web.Application;
+using Microsoft.Web.Administration;
+using Application = Microsoft.Web.Administration.Application;
 
 namespace Whut.AttachTo
 {
@@ -24,30 +28,99 @@ namespace Whut.AttachTo
         {
             base.Initialize();
 
-            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
 
-            this.AddAttachToCommand(mcs, PkgCmdIDList.cmdidWhutAttachToIIS, gop => gop.ShowAttachToIIS, "w3wp.exe");
-            this.AddAttachToCommand(mcs, PkgCmdIDList.cmdidWhutAttachToIISExpress, gop => gop.ShowAttachToIISExpress, "iisexpress.exe");
-            this.AddAttachToCommand(mcs, PkgCmdIDList.cmdidWhutAttachToNUnit, gop => gop.ShowAttachToNUnit, "nunit-agent.exe", "nunit.exe", "nunit-console.exe", "nunit-agent-x86.exe", "nunit-x86.exe", "nunit-console-x86.exe");
+            AddAttachToCommand(mcs, PkgCmdIDList.cmdidWhutAttachToIIS, gop => gop.ShowAttachToIIS, "w3wp.exe");
+            AddAttachToCommand(mcs, PkgCmdIDList.cmdidWhutAttachToIISExpress, gop => gop.ShowAttachToIISExpress, "iisexpress.exe");
+            AddAttachToCommand(mcs, PkgCmdIDList.cmdidWhutAttachToNUnit, gop => gop.ShowAttachToNUnit, "nunit-agent.exe", "nunit.exe", "nunit-console.exe", "nunit-agent-x86.exe", "nunit-x86.exe", "nunit-console-x86.exe");
         }
 
         private void AddAttachToCommand(OleMenuCommandService mcs, uint commandId, Func<GeneralOptionsPage, bool> isVisible, params string[] programsToAttach)
         {
-            OleMenuCommand menuItemCommand = new OleMenuCommand(
-                delegate(object sender, EventArgs e)
-                {
-                    DTE dte = (DTE)this.GetService(typeof(DTE));
-                    foreach (Process process in dte.Debugger.LocalProcesses)
-                    {
-                        if (programsToAttach.Any(p => process.Name.EndsWith(p)))
-                        {
-                            process.Attach();
-                        }
-                    }
-                },
+            var menuItemCommand = new OleMenuCommand((sender, args) => Attach(programsToAttach),
                 new CommandID(GuidList.guidAttachToCmdSet, (int)commandId));
-            menuItemCommand.BeforeQueryStatus += (s, e) => menuItemCommand.Visible = isVisible((GeneralOptionsPage)this.GetDialogPage(typeof(GeneralOptionsPage)));
+
+            menuItemCommand.BeforeQueryStatus += (s, e) => menuItemCommand.Visible = isVisible((GeneralOptionsPage)GetDialogPage(typeof(GeneralOptionsPage)));
             mcs.AddCommand(menuItemCommand);
+        }
+
+        private void Attach(string[] programsToAttach)
+        {
+            if (programsToAttach[0] == "w3wp.exe")
+            {
+                AttachToIIS();
+            } 
+            else
+            {
+                var dte = (DTE)GetService(typeof(DTE));
+                foreach (Process process in dte.Debugger.LocalProcesses)
+                {
+                    if (programsToAttach.Any(p => process.Name.EndsWith(p)))
+                    {
+                        process.Attach();
+                    }
+                }
+            }
+        }
+
+        private void AttachToIIS()
+        {
+            var dte = (DTE)GetService(typeof(DTE));
+
+            foreach (Project project in dte.Solution.Projects)
+            {
+                foreach (object item in (Array)project.ExtenderNames)
+                {
+                    var extend = project.Extender[item.ToString()] as WAProjectExtender;
+                    if (extend == null)
+                    {
+                        continue;
+                    }
+
+                    using (var serverManager = new ServerManager())
+                    {
+                        var uri = new Uri(extend.IISUrl);
+                        var site = serverManager.Sites.FirstOrDefault(x => x.Bindings.Any(y =>
+                                                                                          y.Host == uri.Host &&
+                                                                                          y.EndPoint.Port ==
+                                                                                          uri.Port &&
+                                                                                          y.Protocol == uri.Scheme));
+
+                        if (site == null)
+                        {
+                            continue;
+                        }
+
+                        if (site.Applications.Count > 1)
+                        {
+                            MessageBox.Show("Multiple applications in single site are not supported, attaching to first application pool");
+                        }
+
+                        Application application = site.Applications.FirstOrDefault();
+
+                        if (application == null)
+                        {
+                            MessageBox.Show("Wrong IIS configuration (site does not have any applications)");
+                            return;
+                        }
+
+                        var pool = serverManager.ApplicationPools[application.ApplicationPoolName];
+                        foreach (var workerProcess in pool.WorkerProcesses)
+                        {
+                            foreach (Process process in dte.Debugger.LocalProcesses)
+                            {
+                                if (process.ProcessID == workerProcess.ProcessId)
+                                {
+                                    process.Attach();
+                                    return;
+                                }
+                            }
+                        }
+
+                        MessageBox.Show("Could not find process to attach to, it is not started?");
+                    }
+                }
+            }
         }
     }
 }
